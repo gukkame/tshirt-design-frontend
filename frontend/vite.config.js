0dca98f128f0029acd23afa8ba08788cdf70b904
gukkame <guka245@gmail.com>
2025-03-30 13:45:36 +0300

    Adjusted server conf for railway

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  server: {
+    host: '0.0.0.0', // Bind to all IPs
+    port: process.env.PORT || 8080, // Use Railway's PORT or default to 3000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
